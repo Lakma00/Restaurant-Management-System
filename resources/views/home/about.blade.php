c25beb1277d405ba04694579e4f62a346aa62ba0
Lakma Navodi <142379757+Lakma00@users.noreply.github.com>
2025-07-18 15:24:47 +0530

    Add files via upload

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,15 @@
+<div id="about" class="container-fluid wow fadeIn" id="about"data-wow-duration="1.5s">
+        <div class="row">
+            <div class="col-lg-6 has-img-bg"></div>
+            <div class="col-lg-6">
+                <div class="row justify-content-center">
+                    <div class="col-sm-8 py-5 my-5">
+                        <h2 class="mb-4">About Us</h2>
+                        <p>Welcome to Food Hut, where great food meets modern convenience. We are more than just a restaurant — we’re a complete dining experience designed to fit your lifestyle. With our user-friendly web application, you can easily explore our diverse menu, place orders online, book a table in advance, or get your favorite dishes delivered straight to your doorstep. Whether you're planning a cozy dinner at home or a special meal out with family and friends, Food Hut is here to make it smooth and satisfying. Our chefs are passionate about using fresh, high-quality ingredients to craft meals that not only taste amazing but leave you coming back for more. We value your time, so our system is built to be fast, reliable, and hassle-free. At Food Hut, we believe good food brings people together — and now, we’ve made it easier than ever for you to enjoy it your way.
+
+</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,35 @@
+
+   
+    <!-- page footer  -->
+    <div class="container-fluid bg-dark text-light has-height-md middle-items border-top text-center wow fadeIn">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>EMAIL US</h3>
+                <P class="text-muted">foodhut@gmail.com</P>
+            </div>
+            <div class="col-sm-4">
+                <h3>CALL US</h3>
+                <P class="text-muted">0123456789</P>
+            </div>
+            <div class="col-sm-4">
+                <h3>FIND US</h3>
+                <P class="text-muted">Colombo,Sri Lanka</P>
+            </div>
+        </div>
+    </div>
+    
+    <!-- end of page footer -->
+
+	<!-- core  -->
+    <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+
+    <!-- bootstrap affix -->
+    <script src="assets/vendors/bootstrap/bootstrap.affix.js"></script>
+
+    <!-- wow.js -->
+    <script src="assets/vendors/wow/wow.js"></script>
+    
+   
+    <!-- FoodHut js -->
+    <script src="assets/js/foodhut.js"></script>
